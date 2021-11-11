7780cd6b62c338a7b2031a7f8bc24cf7a32658c6
NicolaLisci <liscinicola@live.it>
2021-11-12 00:30:09 +0100

    Update scully with blog list

--- src/app/blog/blog-routing.module.ts
+++ src/app/blog/blog-routing.module.ts
@@ -2,16 +2,21 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 
 import {BlogComponent} from './blog.component';
+import { ListComponent } from './list/list.component';
 
 const routes: Routes = [
-  {
-    path: ':slug',
-    component: BlogComponent,
-  },
-  {
-    path: '**',
-    component: BlogComponent,
-  }
+	{
+		path: '',
+		component: ListComponent,
+	},
+	{
+		path: ':slug',
+		component: BlogComponent,
+	},
+	{
+		path: '**',
+		component: ListComponent,
+	},
 ];
 
 @NgModule({
--- src/app/blog/list/list.component.ts
+++ src/app/blog/list/list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ScullyRoute, ScullyRoutesService } from '@scullyio/ng-lib';
+import { Observable, map } from 'rxjs';
 
 @Component({
   selector: 'app-list',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
+  public blogPosts$: Observable<ScullyRoute[]>;
 
-  constructor() { }
+	constructor(private _scully: ScullyRoutesService) {}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.blogPosts$ = this._scully.available$.pipe(
+			map((routes: ScullyRoute[]) => {
+				return routes.filter((r: ScullyRoute) => {
+					return r.route.includes('/blog/') && r.route !== '/blog';
+				});
+			}),
+		);
+	}
+}
 
 }
